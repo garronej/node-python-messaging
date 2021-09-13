18a2e000891fccc48bb5a2ab1ed9bd161d0bb645
garronej <joseph.garrone.gj@gmail.com>
2021-09-13 22:07:33 +0200

    tell virtualenv that we wich to use python2 https://github.com/garronej/chan-dongle-extended/issues/3

--- install-python-dep.sh
+++ install-python-dep.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p ./dist/virtual 
-virtualenv ./dist/virtual
+virtualenv -p $(which python2 || which python) ./dist/virtual
 . ./dist/virtual/bin/activate
 ./dist/virtual/bin/python ./dist/virtual/bin/pip install -r ./requirements.txt
